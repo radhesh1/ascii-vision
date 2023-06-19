232141bf31aea7fe23e59c5d3ebd73f7b5fc2c0c
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-20 04:20:49 +0530

    updates

---
+++ script/app.js
@@ -0,0 +1,55 @@
+/*
+ * ASCII Camera
+ * http://radhesh1.github.com/ascii-camera/
+ *
+ * Copyright 2023, Radhesh Goel (http://github.com/radhesh1)
+ * Released under the MIT license
+ */
+
+(function() {
+	var asciiContainer = document.getElementById("ascii");
+	var capturing = false;
+
+	camera.init({
+		width: 160,
+		height: 120,
+		fps: 30,
+		mirror: true,
+
+		onFrame: function(canvas) {
+			ascii.fromCanvas(canvas, {
+				// contrast: 128,
+				callback: function(asciiString) {
+					asciiContainer.innerHTML = asciiString;
+				}
+			});
+		},
+
+		onSuccess: function() {
+			document.getElementById("info").style.display = "none";
+
+			const button = document.getElementById("button");
+			button.style.display = "block";
+			button.onclick = function() {
+				if (capturing) {
+					camera.pause();
+					button.innerText = 'resume';
+				} else {
+					camera.start();
+					button.innerText = 'pause';
+				}
+				capturing = !capturing;
+			};
+		},
+
+		onError: function(error) {
+			// TODO: log error
+		},
+
+		onNotSupported: function() {
+			document.getElementById("info").style.display = "none";
+			asciiContainer.style.display = "none";
+			document.getElementById("notSupported").style.display = "block";
+		}
+	});
+})();
